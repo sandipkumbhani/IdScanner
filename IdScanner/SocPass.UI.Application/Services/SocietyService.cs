2caaa531e055058cdf34d7d961720132922dc45d
RinkeshPatel135 <rinkeshsavaliya2005@gmail.com>
2025-09-17 17:22:34 +0530

    create society ui layer

---
+++ IdScanner/SocPass.UI.Application/Services/SocietyService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocPass.Domain.Model;
+using SocPass.UI.Application.Interface;
+using SocPass.UI.Domain.Interfaces;
+
+namespace SocPass.UI.Application.Services
+{
+    public class SocietyService:ISocietyService
+    {
+        private readonly ISocietyRepository _societyRepository;
+        public SocietyService(ISocietyRepository societyRepository)
+        {
+            _societyRepository = societyRepository;
+        }
+        public async Task<List<Society>> GetAllSocietyAsync()
+        {
+            return await _societyRepository.GetAllSocietyAsync();
+        }
+
+        public async Task<Society> GetSocietyByIdAsync(int? societyId)
+        {
+            return await _societyRepository.GetSocietyByIdAsync(societyId);
+        }
+
+        public async Task<string> AddSocietyAsync(Society society)
+        {
+            return await _societyRepository.AddSocietyAsync(society);
+        }
+
+        public async Task<string> UpdateSocietyAsync(Society society)
+        {
+            return await _societyRepository.UpdateSocietyAsync(society);
+        }
+        public async Task<string> DeleteSocietyAsync(int societyId)
+        {
+            return await _societyRepository.DeleteSocietyAsync(societyId);
+        }
+    }
+}
---
+++ IdScanner/SocPass.UI/Controllers/SocietyController.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using SocPass.Domain.Model;
+using SocPass.UI.Application.Interface;
+using SocPass.UI.Domain.Model;
+
+namespace SocPass.UI.Controllers
+{
+    public class SocietyController : Controller
+    {
+        private readonly ISocietyService _societyService;
+        private GlobalClass _globalClass;
+        public SocietyController(ISocietyService societyService, GlobalClass globalClass)
+        {
+            _societyService = societyService
+                ?? throw new ArgumentNullException(nameof(societyService));
+            _globalClass = globalClass;
+        }
+
+        public async Task<IActionResult> SocietyList()
+        {
+            //if (string.IsNullOrEmpty(_globalClass.Token))
+            //{
+            //    return RedirectToAction("Login", "Login");
+            //}
+            IList<Society> SocietyList = await _societyService.GetAllSocietyAsync();
+            ViewBag.SocietyList = SocietyList;
+            return View("~/Views/Society/SocietyList.cshtml");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddSociety(int? societyId)
+        {
+            //if (string.IsNullOrEmpty(_globalClass.Token))
+            //{
+            //    return RedirectToAction("Login", "Login");
+            //}
+            if (societyId == null)
+            {
+                return View(new Society());
+            }
+            var society = await _societyService.GetSocietyByIdAsync(societyId.Value);
+            return View(society);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSociety(Society society)
+        {
+            //if (string.IsNullOrEmpty(_globalClass.Token))
+            //{
+            //    return RedirectToAction("Login", "Login");
+            //}
+            var userIdClaim = HttpContext.User?.FindFirst("UserId")?.Value;
+            long.TryParse(userIdClaim, out long userId);
+            society.UserId = (int)userId;
+            if (society.SocietyId == 0)
+            {
+                await _societyService.AddSocietyAsync(society);
+
+            }
+            else
+            {
+                await _societyService.UpdateSocietyAsync(society);
+            }
+            return RedirectToAction("SocietyList");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteSociety(int societyId)
+        {
+            //if (string.IsNullOrEmpty(_globalClass.Token))
+            //{
+            //    return RedirectToAction("Login", "Login");
+            //}
+            try
+            {
+                await _societyService.DeleteSocietyAsync(societyId);
+                return RedirectToAction("SocietyList");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = $"Society with ID {societyId} not found: {ex.Message}";
+                return View("Error");
+            }
+        }
+    }
+}
