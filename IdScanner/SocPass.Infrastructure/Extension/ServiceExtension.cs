57767bfc348e6021d09168f3f6f1724dd50a5c42
urvisha <urvisha.dev1018@gmail.com>
2025-09-17 11:56:42 +0530

    CHANGES IN SERVICE FILE

--- IdScanner/SocPass.Infrastructure/Extension/ServiceExtension.cs
+++ IdScanner/SocPass.Infrastructure/Extension/ServiceExtension.cs
@@ -15,13 +15,12 @@ namespace SocPass.Infrastructure.Extension
         {
             services.AddScoped<ILoginRepository, LoginRepository>();
             services.AddScoped<ISocietyRepository, SocietyRepository>();
-            services.AddScoped<IBlockReposityory, BlockReposityory>();
+            services.AddScoped<IBlockRepository, BlockRepository>();
             services.AddScoped<IFlatRepository, FlatRepository>();
             services.AddScoped<IMemberRepository, MemberRepository>();
             services.AddScoped<IMenuMasterRepository, MenuMasterRepository>();
             services.AddScoped<IForgotPasswordDbRepository, ForgotPasswordDbRepository>();
-;            return services;
-            services.AddScoped<IBlockRepository, BlockRepository>();
+;          
             return services;
         }
     }
