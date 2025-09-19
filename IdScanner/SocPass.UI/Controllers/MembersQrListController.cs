03b924fa2ed5c86a515468ba713b31c44dba23b3
urvisha <urvisha.dev1018@gmail.com>
2025-09-19 18:35:49 +0530

    add qrlist

---
+++ IdScanner/SocPass.UI/Controllers/MembersQrListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocPass.UI.Controllers
+{
+    public class MembersQrListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
