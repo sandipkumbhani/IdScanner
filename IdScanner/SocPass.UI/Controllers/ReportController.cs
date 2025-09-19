17693129005c185204d71a9aee6e185513434a32
RinkeshPatel135 <rinkeshsavaliya2005@gmail.com>
2025-09-19 18:38:41 +0530

    create report view

---
+++ IdScanner/SocPass.UI/Controllers/ReportController.cs
@@ -0,0 +1,89 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SocPass.UI.Application.Interface;
+using SocPass.UI.Domain.Model;
+
+namespace SocPass.UI.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ISocietyService _societyService;
+        private readonly IBlockService _blockService;
+        private readonly IFlatService _flatService;
+        private readonly IMemberService _memberService;
+
+        public ReportController(ISocietyService societyService, IBlockService blockService, IFlatService flatService,IMemberService memberService)
+        {
+            _societyService = societyService;
+            _blockService = blockService;
+            _flatService = flatService;
+            _memberService = memberService;
+        }
+
+        // GET: Member Report Page
+        [HttpGet]
+        public async Task<IActionResult> MemberReport()
+        {
+            ViewBag.Societies = await _societyService.GetAllSocietyAsync();
+            return View("/Views/Report/ReportDataList.cshtml");
+        }
+
+        // AJAX: Get Blocks by Society
+        [HttpGet]
+        public async Task<JsonResult> GetBlocksBySociety(int societyId)
+        {
+            var blocks = await _blockService.GetBlockBySocietyId(societyId);
+            return Json(blocks.Select(b => new { blockId = b.BlockId, blockName = b.BlockNumber }));
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetFlatsByBlock(int blockId, string? selectedDate)
+        {
+            var flats = await _flatService.GetFlatByBlockId(blockId);
+            var flatReports = new List<object>();
+
+            DateOnly? filterDate = null;
+            if (!string.IsNullOrEmpty(selectedDate))
+            {
+                DateOnly.TryParseExact(selectedDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var parsedDate);
+                filterDate = parsedDate;
+            }
+
+            foreach (var f in flats)
+            {
+                var members = await _memberService.GetAllMember(f.FlatId);
+
+                var filteredMembers = filterDate.HasValue
+                    ? members.Where(m => m.PassDate == filterDate.Value).ToList()
+                    : members;
+
+                int visitedAdults = filteredMembers.Count(m => m.Visited && !m.IsChild);
+                int visitedChildren = filteredMembers.Count(m => m.Visited && m.IsChild);
+                int pending = f.TotalMember - (visitedAdults + visitedChildren);
+
+                flatReports.Add(new
+                {
+                    f.FlatId,
+                    f.FlatNumber,
+                    f.NumberOfAdult,
+                    f.NumberOfChild,
+                    f.TotalMember,
+                    VisitedAdults = visitedAdults,
+                    VisitedChildren = visitedChildren,
+                    Pending = pending,
+                    HasMembersForDate = filteredMembers.Any()
+                });
+            }
+
+            if (filterDate.HasValue && !flatReports.Any(f => (bool)f.GetType().GetProperty("HasMembersForDate")!.GetValue(f)!))
+            {
+                return Json(new object[0]);
+            }
+
+            return Json(flatReports);
+        }
+
+    }
+}
