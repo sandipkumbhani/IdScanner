27f82185282b79bbff54af5ffa881409605e5ead
urvisha <urvisha.dev1018@gmail.com>
2025-09-22 17:46:11 +0530

    Add memberDetails form

---
+++ IdScanner/IdScanner.UI/Controllers/MemberDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdScanner.UI.Controllers
+{
+    public class MemberDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdScanner/SocPass.UI/Controllers/MemberDetailsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocPass.UI.Controllers
+{
+    public class MemberDetailsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> IsVisited(int memberId)
+        {
+            if (string.IsNullOrWhiteSpace(memberId))
+            {
+                return BadRequest("Member Id is not found.");
+            }
+            var userIdClaim = HttpContext.User?.FindFirst("UserId")?.Value;
+            long.TryParse(userIdClaim, out long loggedInUserId);
+            bool success = await _showTranscriptServices.MarkIsTranscriptAsync(filename, loggedInUserId);
+            if (success)
+            {
+                return RedirectToAction("ShowTranscript", "ShowTranscript");
+            }
+            else
+            {
+                TempData["Error"] = "File not found or already marked.";
+                return RedirectToAction("FileList", "ShowTranscript");
+            }
+        }
+    }
+}
