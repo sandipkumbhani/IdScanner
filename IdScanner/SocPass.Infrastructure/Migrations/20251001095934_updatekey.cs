18f150159217f6488cd2832f4256c7e4dfb0bc8a
urvisha <urvisha.dev1018@gmail.com>
2025-10-07 11:54:01 +0530

    fix UI

---
+++ IdScanner/SocPass.Infrastructure/Migrations/20251001095934_updatekey.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocPass.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatekey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_SocietyData_SocietyDataId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_SocietyDataId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "SocietyDataId",
+                table: "Subscriptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SocietyDataId",
+                table: "Subscriptions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_SocietyDataId",
+                table: "Subscriptions",
+                column: "SocietyDataId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_SocietyData_SocietyDataId",
+                table: "Subscriptions",
+                column: "SocietyDataId",
+                principalTable: "SocietyData",
+                principalColumn: "SocietyDataId");
+        }
+    }
+}
---
+++ IdScanner/SocPass.UI/Controllers/AccessDeniedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocPass.UI.Controllers
+{
+    public class AccessDeniedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
